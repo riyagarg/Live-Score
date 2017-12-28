4802bc684ad37560350725f41187a32ba2f66137
riyagarg <riya.garg@practo.com>
2017-12-29 00:27:06 +0530

    drag functionality added

--- src/script.js
+++ src/script.js
@@ -9,7 +9,7 @@ function Main(){
 
   var url =`https://apifootball.com/api/?action=get_events&from=${today}&to=${today}&APIkey=edc13dc8afa177084e69841f16a4e4a77e189acfe2201ef86a82909ddcb27bc2`;
 
-  this.response={};
+  this.response=[];
 
   this.showBoolean = true;
   this.minBoolean = true;
@@ -20,44 +20,73 @@ function Main(){
   this.init = function() {
     const scope = this;
 
+    this.makeApiCall(url,(response)=> {
+      var modifiedResponse = JSON.parse(response);
+      let flag=false;
+      for(var i=0; i<modifiedResponse.length; i++){
+        if(modifiedResponse[i].match_live === '1'){
+            scope.createDOM(modifiedResponse[i]);
+            scope.response = modifiedResponse[i];
+            flag=true;
+            break;
+        }
+      }
+      if(!flag){
+        this.createNoShowDOM();
+      }
+    });
+
     var button1 =document.getElementById('hideButton');
-    button1.value='Hide'
-    button1.innerHTML='Hide'
-    button1.addEventListener('click',function(event){
-      button1.innerHTML= scope.showBoolean ? 'Show' :'Hide'
-      if(scope.showBoolean){
-      container.innerHTML='';
-      clearInterval(xyz)
-    }else{
-      scope.createDOM(scope.response)
-    }
-      scope.showBoolean = !scope.showBoolean
-  })
+      button1.value='Hide'
+      button1.innerHTML='Hide'
+      button1.addEventListener('click',function(event){
+        button1.innerHTML= scope.showBoolean ? 'Show' :'Hide'
+        if(scope.showBoolean){
+        container.innerHTML='';
+        clearInterval(xyz)
+      }else{
+        if(scope.response.length !== 0){
+          scope.createDOM(scope.response)
+        }else{
+          scope.createNoShowDOM();
+        }
+
+      }
+        scope.showBoolean = !scope.showBoolean
+    });
 
     var button2 =document.getElementById('expandButton');
     button2.innerHTML='Expand'
     button2.addEventListener('click',function(event){
       button2.innerHTML= scope.minBoolean ? 'MiniMize' :'Expand'
       if(scope.minBoolean){
-      container.setAttribute("style",'height: 30vh; max-height:30vh;')
-    } else{
-      container.setAttribute("style",'height: 10vh; max-height:10vh;')
-    }
-      scope.minBoolean = !scope.minBoolean
-  })
-
-    this.makeApiCall(url,(response)=> {
-      var modifiedResponse = JSON.parse(response);
-      for(var i=0; i<modifiedResponse.length; i++){
-        if(modifiedResponse[i].match_live === '1'){
-            scope.createDOM(modifiedResponse[i]);
-            scope.response = modifiedResponse[i]
-        }
-        break;
+        container.setAttribute("style",'height: 30vh; max-height:30vh;')
+      } else{
+        container.setAttribute("style",'height: 10vh; max-height:10vh;')
       }
+        scope.minBoolean = !scope.minBoolean
     });
-
+    window.onload = this.addListeners();
   }
+
+  this.addListeners =function(){
+    document.getElementById('div').addEventListener('mousedown', this.mouseDown);
+    window.addEventListener('mouseup', this.mouseUp);
+  }
+
+  this.mouseUp =function(){
+     window.removeEventListener('mousemove', divMove);
+  }
+  this.mouseDown = function(e){
+    window.addEventListener('mousemove', divMove);
+  }
+  const divMove = function(e){
+    var div = document.getElementById('div');
+    div.style.position = 'absolute';
+    div.style.top = e.clientY + 'px';
+    div.style.left = e.clientX + 'px';
+  }
+
 
   this.makeApiCall = function(theUrl, callback) {
      var xmlHttp = new XMLHttpRequest();
@@ -175,7 +204,6 @@ function Main(){
 
     card.appendChild(imageCard);
 
-    const container = document.getElementById('container');
     container.innerHTML='';
     container.appendChild(card)
   }
@@ -183,18 +211,21 @@ function Main(){
 
   this.createDOM= function(data){
     const scope=this;
-
     var functionArray = [this.createPhaseI, this.createPhaseII, this.createPhaseIII];
-
     var count = 0;
-
-   functionArray[0](data);
+    functionArray[0](data);
      xyz = setInterval(function() {
       count++;
       functionArray[count%3](data);
     },5000);
 
-}
+  }
+
+
+  this.createNoShowDOM = function(){
+
+    container.innerHTML='NO LIVE MATCH DATA FOUND'
+  }
 
 
   this.init();
